d164307a725cbdff5b98af449e11163db864c62e
Maximus <Kharatin85@gmail.com>
2024-12-24 15:08:56 +0200

    rw_visual_2

--- rw_visual.py
+++ rw_visual.py
@@ -2,11 +2,25 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Создай случайное блуждание.
-rw = RandomWalk()
-rw.fill_walk()
-# Нанести на график точки блуждания.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# Создать новые блуждания пока работает программа.
+while True:
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
+
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15, 9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+               edgecolors='none', s=1)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+               s=100)
+    # Спрятать оси
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Сгенерировать новое блуждание? (y/n): ")
+    if keep_running == 'n':
+        break
