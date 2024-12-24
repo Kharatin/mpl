72fe2edf8a786f0773997ae75fb7e09a55e657ff
Maximus <Kharatin85@gmail.com>
2024-12-24 12:49:29 +0200

    simple

--- mpl.py
+++ mpl.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import  matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+squares = [1, 4, 9, 16, 25]
 
+fig, ax = plt.subplots()
+ax.plot(squares)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.show()
