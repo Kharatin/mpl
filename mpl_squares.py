69c5d023b1c909619f0bccca56b04a265d7859a3
Maximus <Kharatin85@gmail.com>
2024-12-24 13:21:53 +0200

    simple style

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,6 +3,7 @@ import  matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
@@ -13,5 +14,6 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Заать розмер шрифта для надписей на розметке.
 ax.tick_params(axis='both', labelsize=14)
+print(plt.style.available)
 
 plt.show()
